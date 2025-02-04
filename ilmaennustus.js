c94a3a8d10459cf49d35c83d96b048dd85b6b36a
Martin Pruus <martin.pruus@tptlive.ee>
2025-02-04 16:18:33 +0200

    printet smth

--- ilmaennustus.js
+++ ilmaennustus.js
@@ -1,7 +1,6 @@
-url = fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.4&lon=24.7")
+weather = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.4&lon=24.7"
 
-function printData() {
-    console.log(url)
-}
-
-printData()
+fetch(weather)
+    .then(response =>{
+        console.log(response)
+    });
