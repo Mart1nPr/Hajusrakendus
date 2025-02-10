63e8665c4529aefb8c63de770591b6a7c5cb041a
Martin Pruus <martin.pruus@tptlive.ee>
2025-02-10 17:33:58 +0200

    Updated ilmaennustus.js

--- ilmaennustus.js
+++ ilmaennustus.js
@@ -1,6 +1,7 @@
-weather = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.4&lon=24.7"
-
-fetch(weather)
-    .then(response =>{
-        console.log(response)
-    });
+fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.4&lon=24.7")
+  .then(response => response.json())
+  .then(data => {
+    const currentTime = data.properties.timeseries[0];
+    const airTemp = currentTime.data.instant.details.air_temperature;
+    console.log(currentTime.time + " " + airTemp + "C");
+  });
