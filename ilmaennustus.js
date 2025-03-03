b821b21a6d888c91d03bc533bbe7a1ca6e70f6a7
Martin Pruus <martin.pruus@tptlive.ee>
2025-03-03 17:04:03 +0200

    Add for loop

--- ilmaennustus.js
+++ ilmaennustus.js
@@ -1,7 +1,14 @@
 fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.4&lon=24.7")
   .then(response => response.json())
   .then(data => {
-    const currentTime = data.properties.timeseries[0];
-    const airTemp = currentTime.data.instant.details.air_temperature;
-    console.log(currentTime.time + " " + airTemp + "C");
+
+    const timeSeries = data.properties.timeseries;
+
+    for (let i = 0; i < 3; i++) {
+      const entry = timeSeries[i];
+      const time = entry.time;
+      const airTemp = entry.data.instant.details.air_temperature;
+      console.log(time + " " + airTemp);
+    }
+
   });
