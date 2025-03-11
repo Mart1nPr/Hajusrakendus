4a6c9cdeeac536dc5c123c3f641539e40642abd9
Martin Pruus <martin.pruus@tptlive.ee>
2025-03-11 15:58:09 +0200

    Print out the file content

--- Auto varuosade HTTP API/ServerScript.js
+++ Auto varuosade HTTP API/ServerScript.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+const fs = require("fs");
+const Papa = require("papaparse");
+const express = require('express');
+const path = require("path");
+const app = express();
 
-app.get('/varuosad', function (req, res) {
-  res.send("Hello API");
+// fs.readFile("LE.txt", "utf-8", (CSVdata) => {
+//  Papa.parse("LE.txt", )
+// });
+
+const data = [];
+
+app.get('/spare-parts', function (req, res) {
+  const carParts = path.join(__dirname, "LE.txt");
+
+  fs.readFile(carParts, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading file");
+    }
+    res.send(data);
+  });
 });
 
 app.listen(3000);
+
+console.log("Live server url: http://localhost:3000/spare-parts");
